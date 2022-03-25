49f9350ae11179f1aae4207096ab604103ad353d
Nikita Kurylev <n.kurilev@gmail.com>
2022-03-25 17:34:34 +0300

    feat: implement dichotomy

--- lab-1/dichotomy.py
+++ lab-1/dichotomy.py
@@ -1,2 +1,12 @@
-def calc(func, a, b, accuracy):
-    return 0
+def calc(func, a, b, precision):
+    while b - a > precision:
+        middle = (a + b) / 2
+        x1 = middle - precision / 3
+        x2 = middle + precision / 3
+        f1 = func(x1)
+        f2 = func(x2)
+        if f1 > f2:
+            a = x1
+        else:
+            b = x2
+    return (a + b) / 2
--- lab-1/main.py
+++ lab-1/main.py
@@ -5,5 +5,5 @@ import goldenSection
 def func(x):
     return math.sin(x) * x ** 3
 
-print(dichotomy.calc(func, -100, 100, 0.1))
+print(dichotomy.calc(func, 13, 20, 0.1))
 print(goldenSection.calc(func, 13, 20, 0.1))
