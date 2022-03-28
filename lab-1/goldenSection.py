0865f4d411923961fcd4a99ad2d054f34b114413
Nikita Kurylev <n.kurilev@gmail.com>
2022-03-28 22:19:01 +0300

    fix: golden section

--- lab-1/goldenSection.py
+++ lab-1/goldenSection.py
@@ -1,12 +1,12 @@
 def calc(func, a, b, precision):
     iteration_count = 0
-    phi = (1 + (5 ** 0.5)) / 2
-    while abs(a - b) > precision:
-        x1 = b - (b - a) / phi
-        x2 = a + (b - a) / phi
+    phi = (5 ** 0.5 - 1) / 2
+    while b - a > precision:
+        x1 = a + (b - a) * phi
+        x2 = b - (b - a) * phi
         y1 = func(x1)
         y2 = func(x2)
-        if y1 >= y2:
+        if y1 < y2:
             a = x1
         else:
             b = x2
