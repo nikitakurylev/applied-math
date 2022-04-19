513a5fda3739cd5740621ce515f5a94b073eb5e6
Nikita Kurylev <n.kurilev@gmail.com>
2022-04-19 14:26:20 +0300

    feat: implement plot

---
+++ lab-2/analysis.py
@@ -0,0 +1,29 @@
+import gradient_descent as gd
+import numpy as np
+import matplotlib.pyplot as plt
+
+fn1 = lambda xy: xy[0] ** 2 + xy[1] ** 3 + xy[0] ** 4
+
+x = gd.gradient_descent(fn1, [50., 50.], 0.001, 1.,)
+xmin = x[0][0]
+ymin = x[0][1]
+xmax = x[0][0]
+ymax = x[0][1]
+x1 = []
+x2 = []
+
+curx = [x[0][0], x[0][1]]
+for i in range(0, len(x)):
+    xmin = min(xmin, x[i][0])
+    ymin = min(ymin, x[i][1])
+    xmax = max(xmax, x[i][0])
+    ymax = max(ymax, x[i][1])
+    x1.append(x[i][0])
+    x2.append(x[i][1])
+plt.plot(x1, x2)
+x1, x2 = np.meshgrid(np.arange(xmin, xmax, 0.1), np.arange(ymin, ymax, 0.1))
+y = fn1([x1,x2])
+
+plt.imshow(y, extent=[xmin,xmax,ymin,ymax], cmap="Greys")
+plt.colorbar()
+plt.show()
