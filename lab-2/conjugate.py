ee26643f1d4facc7ef91f3b18257886fa503ba8d
aa1dar <naidar31@gmail.com>
2022-04-20 21:03:40 +0300

    refactoring code

--- lab-2/conjugate.py
+++ lab-2/conjugate.py
@@ -1,5 +1,7 @@
 import numpy as np
-from math_util import gradient, hessian_matrix, norm, norm_gradient, normalize
+import numdifftools as nd
+from gradient_descent import vector_mod, vector_normalization
+from math_util import norm_gradient
 import goldenSection
 M = 50000
 K = (3 - (5 ** 0.5)) / 2
@@ -15,23 +17,23 @@ def conjugate_method(f, x0, eps, lambda_b):
     while k < M:
         x_k = x_next
         grad_prev = grad
-        grad = gradient(f, x_k)
-        if norm(grad) < eps:
+        grad = nd.Gradient(f)(x_k)
+        if vector_mod(grad) < eps:
             break
-        grad = normalize(grad)
+        grad = vector_normalization(grad)
         k += 1
         b = lambda_b(grad, grad_prev)
         d = -grad + b * d
-        f_min = lambda tmp: f(*(x_k + tmp * d))
-        t = goldenSection.calc(f_min, 0., 10000000., eps)[0];
+        f_min = lambda tmp: f((x_k + tmp * d))
+        t = goldenSection.calc(f_min, 0., 10000000., eps)[0]
         x_next = x_k + t * d
         trajectory.append(x_next)
-        if norm(x_next - x_k) < eps and abs(f(*x_next) - f(*x_k)) < eps:
+        if vector_mod(x_next - x_k) < eps and abs(f(x_next) - f(x_k)) < eps:
             break
     return trajectory
 
 
 def conjugate_gradient_method(f, x0, eps):
     return conjugate_method(
-        f, x0, eps, lambda grad, grad_prev: norm(grad) ** 2 / norm(grad_prev) ** 2
+        f, x0, eps, lambda grad, grad_prev: vector_mod(grad) ** 2 / vector_mod(grad_prev) ** 2
     )
