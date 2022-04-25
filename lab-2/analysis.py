f137908e5d9c8a97f85c6a094a25d4d69f77c0f0
Nikita Kurylev <n.kurilev@gmail.com>
2022-04-26 01:24:17 +0300

    fix: change tests

--- lab-2/analysis.py
+++ lab-2/analysis.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import conjugate
 
 def draw(ax, x, fn1, name):
+    print(name)
     xmin = x[0][0]
     ymin = x[0][1]
     xmax = x[0][0]
@@ -24,17 +25,19 @@ def draw(ax, x, fn1, name):
         x2.append(x[i][1])
         y.append(fn1(x[i]))
     ax.plot(x1, x2, y, label=name)
+    print()
 
 def drawmesh(ax, fn1, xmin, xmax, ymin, ymax):
     x1, x2 = np.meshgrid(np.arange(xmin, xmax, 0.5), np.arange(ymin, ymax, 0.5))
     y = fn1([x1,x2])
     ax.plot_wireframe(x1, x2, y, linewidth=0.5, color="red")
 
-fn1 = lambda xy: 22 * ((xy[0]-100) ** 4) + 8 * (xy[1] ** 4)
+#fn1 = lambda xy: 22 * ((xy[0]-100) ** 4) + 8 * (xy[1] ** 4)
+fn1 = lambda xy: xy[0] ** 2 + xy[1] ** 2
 fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 matplotlib.rcParams['legend.fontsize'] = 10
-drawmesh(ax, fn1, 0, 120, -10, 20)
-startpoint = [10., 10.]
+drawmesh(ax, fn1, -20, 20, -20, 20)
+startpoint = [7., 15.]
 draw(ax, gd.gradient_descent(fn1, startpoint, 0.001, 1., gd.const_step), fn1, 'const_step')
 draw(ax, gd.gradient_descent(fn1, startpoint, 0.001, 1., gd.splitting_method), fn1, 'splitting_method')
 draw(ax, gd.gradient_descent(fn1, startpoint, 0.001, 1., gd.golden_section), fn1, 'golden_section')
