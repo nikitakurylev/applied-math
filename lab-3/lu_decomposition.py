6f716ed8a1d553a0ba58398acf308d31181c3031
aa1dar <naidar31@gmail.com>
2022-05-16 01:10:22 +0300

    feat: added LU-decomposition and algorithm for solving a matrix equation with it

---
+++ lab-3/lu_decomposition.py
@@ -0,0 +1,77 @@
+import numpy as np
+import scipy
+
+def solution(A, b):
+    L, U = lu_decomposition(A)
+    #Решаем L(Ux) = b
+    #1) Ly = b
+    y = lower_trivial_solution(L, b)
+    #2) Ux = y
+    x = upper_trivial_solution(U, y)
+    return x
+
+def lu_decomposition(A):
+    N = A.shape[0]
+    #init empty matrix
+    L = scipy.sparse.__dict__["lil_matrix"]((N, N))
+    U = L.copy()
+
+    for i in range(N):
+
+        # U
+        for k in range(i, N):
+
+            # sum(L(i, j) * U(j, k)) (j,i-1)
+            sum = 0
+            for j in range(i):
+                sum += (L[i,j] * U[j,k])
+            # U(i, k)
+            U[i,k] = A[i,k] - sum
+
+        # L
+        for k in range(i, N):
+            if i == k:
+                L[i,i] = 1  # Главн. диагональ = 1
+            else:
+
+                # sum(L(k, j) * U(j, i)) (j,i-1)
+                sum = 0
+                for j in range(i):
+                    sum += (L[k, j] * U[j, i])
+
+                # L(k, i)
+                L[k,i] = ((A[k,i] - sum) / U[i,i])
+
+    return L.tocsr(), U.tocsr()
+
+
+def lower_trivial_solution(A, b):
+    #Ax=b, где A - нижняя треуг. матрица
+    x = np.zeros(len(b))
+    x[0] = b[0]
+
+    for i in range(1, len(b)):
+        x[i] = b[i] - A[i] * x
+
+    return x
+
+
+def upper_trivial_solution(A, b):
+    #Ax=b, где A - верхняя треуг. матрица
+    N = len(b)
+    x = np.zeros(N)
+    x[-1] = b[-1] / A[-1, -1]
+
+    for i in reversed(range(N - 1)):
+        x[i] = (b[i] - A[i] * x) / A[i, i]
+
+    return x
+
+def inverse_by_lu_decomposition(A):
+    E = np.eye(A.shape[0])
+    Inv = []
+    for e in E:
+        x = solution(A, e)
+        Inv.append(x)
+    return np.array(Inv).transpose()
+
--- lab-3/main.py
+++ lab-3/main.py
@@ -1,11 +1,14 @@
 import scipy.sparse
 import numpy as np
 import iteration_method as im
+import lu_decomposition as lu
 
 n = 3
 matrix = scipy.sparse.__dict__["lil_matrix"]((n, n))
 
-a = np.array([[5, -1, 3], [1, -4, 2], [2, -1, 5]])
+a = np.array([[5, -1, 3],
+              [1, -4, 2],
+              [2, -1, 5]])
 b = np.array([5, 20, 10])
 
 for i in range(n):
@@ -14,3 +17,8 @@ for i in range(n):
 
 matrix = matrix.tocsr()
 print(im.seidel_method(matrix, b))
+print((lu.lu_decomposition(matrix)[0]).toarray())
+print((lu.lu_decomposition(matrix)[1]).toarray())
+print((lu.lu_decomposition(matrix)[0]*lu.lu_decomposition(matrix)[1]).toarray())
+print(lu.solution(matrix, b))
+print(lu.inverse_by_lu_decomposition(matrix))
